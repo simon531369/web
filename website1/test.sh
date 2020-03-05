47bd82e90703c8008b6316b26e916800ccc35d66
simon531369 <simon531369@gmail.com>
2020-03-05 15:39:50 +0000

    commit3

--- website1/test.sh
+++ website1/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get upadte
 sudo apt-get -y install apache2
-sudo cp -rf /home/ubuntu/jenkins/workspace/job1/website1/* /var/www/html
+sudo cp -rf /home/ubuntu/jenkins/workspace/webjob/website1/* /var/www/html
 sudo service apache2 restart
